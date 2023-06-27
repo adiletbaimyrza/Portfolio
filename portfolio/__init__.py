2a25af686213e7eb4e4b5ad3ee751bb9665e8e58
Adilet Baimyrza uulu <adiletbaimyrza@gmail.com>
2023-06-27 20:10:43 +0200

    everything is setup

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Habit tracking app with Python and MongoDB",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name": "Personal finance tracking app with React",
+        "thumb": "img/personal-finance.png",
+        "hero": "img/personal-finance.png",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name": "REST API Documentation with Postman and Swagger",
+        "thumb": "img/rest-api-docs.png",
+        "hero": "img/rest-api-docs.png",
+        "categories": ["writing"],
+        "slug": "api-docs",
+        "prod": "https://udemy.com",
+    },
+]
+
+slug_to_project = {project["slug"] for project in projects}
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 @app.route("/about")
 def about():
@@ -15,3 +43,10 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    
+    return render_template(f'project_{slug}.html')
